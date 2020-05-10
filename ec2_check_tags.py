135d6863d32627aa98cae251f1690c222efd3407
saha-rajdeep <raj.x.saha@gmail.com>
2020-05-09 23:58:04 -0400

    Lambda for Boto3 example

---
+++ ec2_check_tags.py
@@ -0,0 +1,48 @@
+import json
+import boto3
+
+ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    #print(event)
+    ec2_instance_id=event['detail']['instance-id']
+    
+    # Put Logic
+    tag_response= ec2.describe_tags(
+    Filters=[
+        {
+            'Name': 'resource-id',
+            'Values':[ec2_instance_id],
+        },
+    ],
+    )
+
+    alltags=tag_response['Tags']
+    
+    flag='STOP'
+    for item in alltags:
+        print(item['Key'])
+        if item['Key']=='SPECIAL_EXCEPTION':
+            flag='DONT_STOP'
+            break
+            
+    print(flag)    
+    
+    # Decision Making
+    
+    if flag=='STOP':
+        ec2.stop_instances(InstanceIds=[ec2_instance_id])
+        snsarn="arn:aws:sns:us-east-1:719217631821:drcft1221-BadImageSNSTopic-S33BSS9PJJXM"
+        errormsg="EC2 "+ ec2_instance_id + " stopped"
+        snsresponse=sns.publish(TopicArn=snsarn,
+                                Message=errormsg,
+                                Subject="EC2 Violated Company Policy")
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
